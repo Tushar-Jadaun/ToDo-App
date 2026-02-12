458ee63ef01af88897df394f5af2a51752c97d07
Tushar-Jadaun <tusharjadaun2001485@gmail.com>
2026-02-12 07:58:18 +0530

    completed app.jsx code

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Todoinput from './components/Todoinput'
 import Todoitem from './components/Todoitem'
 import Todolist from './components/Todolist'
 
 
 export default function App() {
+  const [todos,setTodos]=useState([]);
+  const addTodo = (text)=>{
+      if(!text.trim())return;
+      setTodos([...todos,{text,completed:false}]);
+  };
+  const deleteTodo=(index)=>{
+     setTodos(todos.filter((_,i)=>i!==index))
+  };
+  const toggleTodo=(index)=>{
+    setTodos(todos.map((todo,i)=>i==index ?{...todo,completed:!todo.completed}:todo))
+  }
   return (
     <div className='min-h-screen bg-gray-400 flex justify-center items-center'>
       <div className='h-80 w-80 bg-gray-300 rounded-2xl'>
             <h1 className='text-center rounded-2xl border p-2'>ToDo App 📝</h1>
             <div >
                <Todoinput addTodo={addTodo}/>
-               <Todolist todo={todo} delete={deleteTodo} toggleTodo={toggleTodo}/>   
+               <Todolist todo={todos} delete={deleteTodo} toggleTodo={toggleTodo}/>   
             </div>
       </div>
     </div>
