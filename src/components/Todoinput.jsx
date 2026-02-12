688ec573ce27b55ff45c998a631356c5efb343dd
Tushar-Jadaun <tusharjadaun2001485@gmail.com>
2026-02-12 08:11:35 +0530

    todoinput code is ready

--- src/components/Todoinput.jsx
+++ src/components/Todoinput.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Todoinput() {
+export default function Todoinput({addTodo}) {
+  const[todo,setTodo]=useState([]);
+  const addingTodo = ()=>{
+    addTodo(todo);
+    setTodo("");
+  }
   return (
-    <div>Todoinput</div>
+    <div>
+         <input 
+         placeholder='Enter the input'
+         value={todo}
+         onChange={(e)=>setTodo(e.target.value)}
+         className='p-2 border rounded flex-1'/>
+         <button className='text-red-50 bg-white rounded-2xl px-4' onClick={addingTodo}>Add</button>
+    </div>
   )
 }
